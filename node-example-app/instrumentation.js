e70931c75a7b203aeddc039d9eed920d2e6dfdce
Gabriel Koch <7779684+elessar-ch@users.noreply.github.com>
2024-01-30 18:16:53 +0100

    Add and use docker image for node example app

--- node-example-app/instrumentation.js
+++ node-example-app/instrumentation.js
@@ -19,13 +19,13 @@ require('dotenv').config()
 const sdk = new opentelemetry.NodeSDK({
     traceExporter: new OTLPTraceExporter({
         // optional - default url is http://localhost:4318/v1/traces
-        url: 'http://localhost:4318/v1/traces',
+        url: process.env.OTEL_COLLECTOR_TRACES_URL || 'http://localhost:4318/v1/traces',
         // optional - collection of custom headers to be sent with each request, empty by default
         headers: {},
     }),
     metricReader: new PeriodicExportingMetricReader({
         exporter: new OTLPMetricExporter({
-            url: 'http://localhost:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
+            url: process.env.OTEL_COLLECTOR_TRACES_URL || 'http://localhost:4318/v1/metrics', // url is optional and can be omitted - default is http://localhost:4318/v1/metrics
             headers: {}, // an optional object containing custom headers to be sent with each request
             concurrencyLimit: 1, // an optional limit on pending requests
         }),
